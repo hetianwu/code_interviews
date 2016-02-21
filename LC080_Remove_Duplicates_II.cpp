26e228d40664884508b981f5f7c7c7cd61e20b24
HetianWu <wuhetian@hotmail.com>
2016-02-21 05:08:18 -0500

    add LC080

---
+++ LC080_Remove_Duplicates_II.cpp
@@ -0,0 +1,34 @@
+/*
+Problem taken from
+https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+Remove Duplicates from Sorted Array II:
+
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
+*/
+
+int removeDuplicates(vector<int>& nums) {
+        int size = nums.size();
+        if(size<3){
+            return size;
+        }
+        int next_index = 2;
+        
+        int prepre = nums[0];
+        int pre = nums[1];
+        for(int i = 2;i<size;i++){
+            if(!(nums[i] == pre && nums[i] == prepre)){
+                nums[next_index] = nums[i];
+                pre = nums[next_index];
+                prepre = nums[next_index-1];
+                next_index++;
+            }
+            
+        }
+        return next_index--;
+    }
---
+++ LC283_Move_Zeroes.cpp
@@ -0,0 +1,35 @@
+/*
+
+Problem is taken from:
+https://leetcode.com/problems/move-zeroes/
+
+Leet Code 283.Move Zeros
+Problem Statement:
+
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+
+
+*/
+
+void moveZeroes(vector<int>& nums) {
+    int size = nums.size();
+    int next_index = 0;
+    int num_int = 0;
+    for(int i = 0; i<nums.size();i++){
+        if(nums[i] != 0){
+            nums[next_index] = nums[i];
+            next_index++;
+        }else{
+            num_int++;
+        }
+    }
+    for(int i = 0; i<num_int;i++){
+        nums[size-1-i] = 0;
+    }
+}

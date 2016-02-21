f40895b4b77b124bdf5012f5339ad02ed2635636
hetianwu <wuhetian@hotmail.com>
2016-02-21 04:04:32 -0500

    Update twosum.cpp

--- twosum.cpp
+++ twosum.cpp
@@ -6,6 +6,8 @@ You may assume that each input would have exactly one solution.
 */
 
 vector<int> twoSum(vector<int>& nums, int target) {
+  // first int is the actual number
+  // second int is the index of the number in the vector nums.
   map<int,int> mymap;
   int size = nums.size();
   for(int i = 0; i<size;i++){
